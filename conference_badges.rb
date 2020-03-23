bb6706167df17d42cf4e98feb1d7dd5fb2143939
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-23 16:44:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ def batch_badge_creator(attendees)
   attendee_messages = []
   attendees.each do |person|
     attendees[person] = "Hello, my name is #{person}"
-    attendee_messages << antendees[person]
+    attendee_messages << anttendees[person]
   end
   return attendee_messages
 end
