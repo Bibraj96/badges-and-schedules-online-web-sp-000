9e2c23b38f71a677faaef55b54e5fb432c6af2c6
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-23 16:48:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,11 +3,21 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendee_messages = []
-  attendees.each do |person|
+def batch_badge_creator(speakers)
+  speaker_messages = []
+  speakers.each do |person|
     message = badge_maker(name)
-    attendee_messages << message
+    speaker_messages << message
   end
-  return attendee_messages
+  return speaker_messages
+end
+
+def batch_badge_creator(speakers)
+  # returns an array of badge messages, using badge_maker
+  badge_messages = []
+  speakers.each do |speaker|
+    message = badge_maker(speaker)
+    badge_messages << message
+  end
+  badge_messages
 end
